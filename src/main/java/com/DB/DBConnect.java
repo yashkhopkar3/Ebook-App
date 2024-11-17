66267a232e36cce38c07f5669a8ffcc43a1e04c8
yashkhopkar3 <ykhopkar3@gmail.com>
2024-11-17 22:40:22 +0530

    changes

--- src/main/java/com/DB/DBConnect.java
+++ src/main/java/com/DB/DBConnect.java
@@ -8,7 +8,7 @@ public class DBConnect {
 	public static Connection getConn(){
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/BookMart","root","avantika");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/BookMart","root","yash");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
